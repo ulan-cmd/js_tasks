a15a69274bf949095803dca668993f9183785a0b
ulancho <ulan.five@gmail.com>
2022-09-29 20:16:18 +0600

    feat: 27-09

---
+++ 27-09/script.js
@@ -0,0 +1,44 @@
+/*
+alert('Hello, world')
+alert('Привет, мир 1')
+alert('Привет, мир 2')
+alert('Привет, мир 3')
+*/
+
+/*
+alert('Error');
+[1, 2].forEach(alert);
+*/
+
+/*
+let user_name = 'Ulan';
+let user_age = 77;
+var car = 'BMW';
+const cars_color = 'black';
+user_name = 'Sasha';
+car = 'Mercedes';
+
+alert(car);
+*/
+
+/*let message = 'Hello';
+message = 'World';
+
+alert(message);*/
+
+
+const user = 'Ulan';
+let return1 = 55;
+alert(return1);
+
+
+
+
+
+
+
+
+
+
+
+
